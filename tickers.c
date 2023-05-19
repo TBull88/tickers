94ba04f9f582e0c615babb3b15d73b92c755f53e
timothybullington <timothy.g.bullington@cybbh.space>
2023-05-19 14:42:24 -0400

    end of day

--- tickers.c
+++ tickers.c
@@ -3,17 +3,25 @@
 #include <string.h>
 
 char get_user_input(void) {
-        char buffer[5];
+        char user_input[5];
         char *end_ptr;
         printf("Enter the ticker for the company you wish to display ==> ");
-        fgets(buffer, 5, stdin);
-        // printf("%s\n", buffer);
-        
+        fgets(user_input, 5, stdin);
+        return user_input;
+
+
 }
 
-// void find_ticker_symbol(char *tickers) {
+void find_ticker_symbol(char *companies, char *user_input) {
+        char *company_name[30];
+        char delim[] = "\n:";
+        int i = 0;
+        company_name[i] = strtok(companies, delim);
 
-// }
+        while(company_name[i] != NULL) {
+                company_name[++i] = strtok(NULL, delim);
+        }
+}
 
 int main(void) {
         FILE *stream;
@@ -27,10 +35,10 @@ int main(void) {
         long file_size = ftell(stream); // file size = 221
         // printf("%ld\n", file_size);
         fseek(stream, 0, SEEK_SET); // Move pointer back to start of file
-        char tickers[file_size];
-        int count = fread(tickers, sizeof(char), file_size, stream);
+        char companies[file_size];
+        int count = fread(companies, sizeof(char), file_size, stream);
         fclose(stream);
-        tickers[count] = '\0';
+        companies[count] = '\0';
         // printf("%s\n", tickers);
-        get_user_input();
+        char user_input = get_user_input();
 }
